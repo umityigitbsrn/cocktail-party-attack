f60b43f98eb51255cb09e69ee9639a0f6b668ed1
umityigitbsrn <umityigitbsrn@gmail.com>
2023-12-21 15:24:32 -0800

    unexpected results putting them to save them for later I will adjust the repo again but I need to save them

--- data.py
+++ data.py
@@ -22,7 +22,7 @@ def load_cifar10_dataloaders(path, batch_size, transform):
 
 def whitening_transformation(gradient_matrix):
     # centering data - zero mean
-    transformed_matrix = gradient_matrix - torch.mean(gradient_matrix, dim=0)
+    transformed_matrix = torch.complex(gradient_matrix - torch.mean(gradient_matrix, dim=0), torch.zeros(gradient_matrix.size()))
 
     # finding cov matrix
     covariance_matrix = transformed_matrix.T
@@ -32,24 +32,24 @@ def whitening_transformation(gradient_matrix):
 
     # eigen decomposition
     eigvals, eigvectors = torch.linalg.eig(covariance_matrix)
-    eigvals, eigvectors = eigvals.real, eigvectors.real
-    eigvals[eigvals < 0] = 0
-    assert not ((torch.any(torch.isnan(eigvals)).item()) or (torch.any(torch.isinf(eigvals)).item())), \
-        'There are NaN or Inf values in eigenvalue array'
+    # eigvals, eigvectors = eigvals.real, eigvectors.real
+    # eigvals[eigvals < 0] = 1e-10
+    # assert not ((torch.any(torch.isnan(eigvals)).item()) or (torch.any(torch.isinf(eigvals)).item())), \
+    #     'There are NaN or Inf values in eigenvalue array'
     sqrt_eigvals = torch.sqrt(eigvals)
-    assert not ((torch.any(torch.isnan(sqrt_eigvals)).item()) or (torch.any(torch.isinf(sqrt_eigvals)).item())), 'There are NaN or Inf values in sqrt eigenvalue array'
+    # assert not ((torch.any(torch.isnan(sqrt_eigvals)).item()) or (torch.any(torch.isinf(sqrt_eigvals)).item())), 'There are NaN or Inf values in sqrt eigenvalue array'
     inverse_sqrt_eigvals = (1 / sqrt_eigvals)
-    inverse_sqrt_eigvals = torch.nan_to_num(inverse_sqrt_eigvals, posinf=10)
-    assert not ((torch.any(torch.isnan(inverse_sqrt_eigvals)).item()) or (torch.any(torch.isinf(inverse_sqrt_eigvals)).item())), 'There are NaN or Inf values in inverse sqrt eigenvalue array'
+    inverse_sqrt_eigvals[torch.isnan(inverse_sqrt_eigvals.real)] = 0.+0.j
+    # assert not ((torch.any(torch.isnan(inverse_sqrt_eigvals)).item()) or (torch.any(torch.isinf(inverse_sqrt_eigvals)).item())), 'There are NaN or Inf values in inverse sqrt eigenvalue array'
 
     # pca whitening
     pca_whitening = (torch.diag(inverse_sqrt_eigvals) @ eigvectors.T @ transformed_matrix.T).T
     # pca_whitening = torch.nan_to_num(pca_whitening)
-    assert not ((torch.any(torch.isnan(pca_whitening)).item()) or (torch.any(torch.isinf(pca_whitening)).item())), 'There are NaN or Inf values in pca whitened array'
+    # assert not ((torch.any(torch.isnan(pca_whitening)).item()) or (torch.any(torch.isinf(pca_whitening)).item())), 'There are NaN or Inf values in pca whitened array'
 
     # zca_whitening
     zca_whitening = (eigvectors @ torch.diag(inverse_sqrt_eigvals) @ eigvectors.T @ transformed_matrix.T).T
     # zca_whitening = torch.nan_to_num(zca_whitening)
-    assert not ((torch.any(torch.isnan(zca_whitening)).item()) or (torch.any(torch.isinf(zca_whitening)).item())), 'There are NaN or Inf values in zca whitened array'
+    # assert not ((torch.any(torch.isnan(zca_whitening)).item()) or (torch.any(torch.isinf(zca_whitening)).item())), 'There are NaN or Inf values in zca whitened array'
 
     return pca_whitening, zca_whitening
