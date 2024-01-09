be2a9b45203175de0f13c392d5b92a3f844820ba
umityigitbsrn <umityigitbsrn@gmail.com>
2024-01-08 16:05:42 -0800

    minor update

--- 010824_fc2_tiny_imagenet_bs_16_lpips_hpo.py
+++ 010824_fc2_tiny_imagenet_bs_16_lpips_hpo.py
@@ -1,7 +1,7 @@
 from hyperparameter_optimization import minimize_lpips
 
 batch_size = 16
-num_of_trials = 2
+num_of_trials = 200
 minimize_lpips('./model_config/fc2_cocktail_party_tiny_imagenet_instance.json',
                './checkpoints/010424_fc2_cocktail_party_tiny_imagenet_pretraining_wout_bias_wout_normalization.pth',
                'tiny-imagenet', './data', batch_size, 200,
