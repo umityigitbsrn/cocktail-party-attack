f2e3b5b8865741e93ce605a5094c13b32ac12b5b
umityigitbsrn <umityigitbsrn@gmail.com>
2024-01-17 15:03:16 -0800

    pretraining feature is added to cpa class

--- cocktail_party_attack/cpa.py
+++ cocktail_party_attack/cpa.py
@@ -1,5 +1,9 @@
 from hyperparameter_optimization import maximize_psnr, minimize_lpips, minimize_lpips_for_specific_image_with_id
 from attack import cocktail_party_attack
+from train import train_all
+from data import load_cifar100_dataloaders, load_mnist_dataloaders, load_tiny_imagenet_dataloaders, load_cifar10_dataloaders
+import torchvision.transforms as transforms
+import torch
 
 class CocktailPartyAttack(object):
     def __init__(self, model_config, checkpoint_path, data_type, data_path, batch_size, *args, return_specific_with_id=None,
@@ -28,6 +32,9 @@ class CocktailPartyAttack(object):
 
         # attack result dict
         self.attack_result_dict = None
+
+        # if pretraining happens
+        self.train_stats = None
 
 
     def attack(self, verbose=True, plot_shape=None,
@@ -80,3 +87,25 @@ class CocktailPartyAttack(object):
         self.attack(verbose=verbose, plot_shape=plot_shape, save_results=save_results, save_json=save_json,
                     save_figure=save_figure, plot_verbose=plot_verbose,
                     save_estimations_and_references=save_estimations_and_references)
+        
+    def pretrain_model(self, optimizer_type, learning_rate, criterion_type, num_epochs, batch_size, save_interval=200, linearize=False, fetch_from_dict=False, batch_sampler=False):
+        device = 'cuda:{}'.format(self.device_number) if torch.cuda.is_available() else 'cpu'
+
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+        if fetch_from_dict:
+            if self.data_type == 'tiny-imagenet':
+                train_loader, val_loader = load_tiny_imagenet_dataloaders(batch_size, batch_sampler=batch_sampler)
+        else:
+            if self.data_type == 'cifar10':
+                train_loader, val_loader = load_cifar10_dataloaders(self.data_path, batch_size, transform, batch_sampler=batch_sampler)
+            elif self.data_type == 'mnist':
+                train_loader, val_loader = load_mnist_dataloaders(self.data_path, batch_size, transform, batch_sampler=batch_sampler)
+            elif self.data_type == 'cifar100':       
+                train_loader, val_loader = load_cifar100_dataloaders(self.data_path, batch_size, transform, batch_sampler=batch_sampler)
+
+        self.train_stats = train_all(self.model_config, optimizer_type, learning_rate, criterion_type, num_epochs,
+                                     train_loader, val_loader=val_loader, device=device, save_path=self.checkpoint_path,
+                                     save_interval=save_interval, linearize=linearize, fetch_from_dict=fetch_from_dict)
